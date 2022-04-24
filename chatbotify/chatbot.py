ed76f5b3ca2bb3ccd56101cb045b87b3a340c00e
yugantara <87812408+yugantara@users.noreply.github.com>
2022-04-24 10:46:40 +0530

    Add files via upload

---
+++ chatbotify/chatbot.py
@@ -0,0 +1,19 @@
+from chatterbot import ChatBot
+from chatterbot.trainers import ListTrainer
+from chatterbot.trainers import ChatterBotCorpusTrainer
+# Creating ChatBot Instance
+CB = ChatBot('ChatBot')
+ # Training with Personal Ques & Ans
+conversation = [
+    "Hello",
+    "Hi there!",
+    "How are you doing?",
+    "I'm doing great.",
+    "You're welcome.",
+    "I am Dveloped by Team Chatbotify"
+]
+trainer = ListTrainer(CB)
+trainer.train(conversation)
+# # Training with English Corpus Data
+trainer_corpus = ChatterBotCorpusTrainer(CB)
+trainer_corpus.train('chatterbot.corpus.english')
